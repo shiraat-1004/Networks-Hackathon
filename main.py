7a1dac1cef6de3dcbab8e0d8f5f991486ad3b2f8
shiraat-1004 <shiraat@post.bgu.ac.il>
2026-01-14 18:59:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-"""
-main.py - Runs BOTH server and client automatically.
-This file is for convenience only.
-The assignment requirements are still fully satisfied by server.py and client.py.
-"""
-
 from __future__ import annotations
 
 import threading
@@ -18,28 +12,41 @@ from client import BlackjackClient
 def main():
     enable_ansi_on_windows()
 
-    TEAM_NAME = "Blackijecky"  # change if needed
+    TEAM_NAME = "Novi&Chinno"
 
     print(banner("🃏 BLACKIJECKY 🃏"))
     print(color("[MAIN] Starting server and client automatically", C.CYAN))
     print(color("[MAIN] You can always run server.py and client.py separately if required", C.GRAY))
+    ans = input(color("[MAIN] Press c to run client separately , s to run server separately or ENTER to run both:", C.YELLOW))
+    if ans == "s":
+        # Start server in background thread
+        server = BlackjackServer(TEAM_NAME)
+        server_thread = threading.Thread(target=server.start, daemon=True)
+        server_thread.start()
+        time.sleep(0.4)
+        input("Type EXIT to stop server.")
 
-    # Start server in background thread
-    server = BlackjackServer(TEAM_NAME)
-    server_thread = threading.Thread(target=server.start, daemon=True)
-    server_thread.start()
-
-    # Give server time to bind sockets and start broadcasting offers
-    time.sleep(0.4)
-
-    # Start client in foreground
-    client = BlackjackClient(TEAM_NAME)
-    try:
+    elif ans == "c":
+        client = BlackjackClient(TEAM_NAME)
         client.client_loop()
-    finally:
-        # If client exits, stop server as well (same process)
-        server.stop()
-        print(color("[MAIN] Server stopped. Goodbye 👋", C.YELLOW))
+    else:
+
+        # Start server in background thread
+        server = BlackjackServer(TEAM_NAME)
+        server_thread = threading.Thread(target=server.start, daemon=True)
+        server_thread.start()
+
+        # Give server time to bind sockets and start broadcasting offers
+        time.sleep(0.4)
+
+        # Start client in foreground
+        client = BlackjackClient(TEAM_NAME)
+        try:
+            client.client_loop()
+        finally:
+            # If client exits, stop server as well (same process)
+            server.stop()
+            print(color("[MAIN] Server stopped. Goodbye 👋", C.YELLOW))
 
 
 if __name__ == "__main__":
